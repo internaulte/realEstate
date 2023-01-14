499ebc98aee66f7edcc76fc62d44e164618202f6
Laurent Nault <laurent.nault@igrafx.com>
2023-01-14 22:44:15 +0100

    test: add new test cases

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -5,7 +5,7 @@ import domain.entities.utils.types.{Latitude, Longitude, PropertyType}
 import zio.prelude.newtypes.Natural
 
 import concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.Duration
 
 @main
@@ -22,13 +22,55 @@ def main(): Unit = {
     description = "a test house"
   )
 
-  val result = PropertiesController.instance.saveNewProperty(newProperty).map(_ => println("end")).recover {
-    case error: Throwable =>
-      println(error.getMessage)
-      println(error.getStackTrace.mkString("Array(", ", ", ")"))
-  }
+  val controller = PropertiesController.instance
 
   // should print email1@email1.com and email2@email2.com
 
-  Await.result(result, Duration.Inf)
+  val otherNewProperty = PropertyDto(
+    propertyType = PropertyType.House,
+    position = Point(latitude = Latitude(5.2), longitude = Longitude(4.9)),
+    priceInMillis = Natural(300000000),
+    surfaceInMeterSquare = Natural(90),
+    numberOfRooms = Natural(4),
+    hasGarage = true,
+    hasGarden = false,
+    description = "a test house"
+  )
+
+  // should print email1@email1.com and email3@email3.com
+
+  val thirdNewProperty = PropertyDto(
+    propertyType = PropertyType.House,
+    position = Point(latitude = Latitude(5.2), longitude = Longitude(4.9)),
+    priceInMillis = Natural(300000000),
+    surfaceInMeterSquare = Natural(90),
+    numberOfRooms = Natural(4),
+    hasGarage = true,
+    hasGarden = true,
+    description = "a test house"
+  )
+
+  // should print email1@email1.com only
+
+  val finalResult: Future[Unit] = {
+    for {
+      _ <- controller.saveNewProperty(newProperty).map(_ => println("end first property")).recover {
+        case error: Throwable =>
+          println(error.getMessage)
+          println(error.getStackTrace.mkString("Array(", ", ", ")"))
+      }
+      _ <- controller.saveNewProperty(otherNewProperty).map(_ => println("end other property")).recover {
+        case error: Throwable =>
+          println(error.getMessage)
+          println(error.getStackTrace.mkString("Array(", ", ", ")"))
+      }
+      _ <- controller.saveNewProperty(thirdNewProperty).map(_ => println("end other property")).recover {
+        case error: Throwable =>
+          println(error.getMessage)
+          println(error.getStackTrace.mkString("Array(", ", ", ")"))
+      }
+    } yield ()
+  }
+
+  Await.result(finalResult, Duration.Inf)
 }
